5d86436a637a978a8027e36c4e3a2bb07aa84c36
Luong Quang Manh <luongquangmanh85@gmail.com>
2018-11-28 12:22:22 +0700

    Add path field. Code refactoring. Recursive by default

--- src/stat-writer.js
+++ src/stat-writer.js
@@ -5,40 +5,37 @@ const path = require('path')
 module.exports = class StatWriter {
   constructor(dir) {
     this.dir = dir
-    this.stat = {
-      name: this.dir,
-      children: []
-    }
+    this.stat = {}
   }
 
-  async parse() {
-    try {
-      const names = await fs.readdir(this.dir)
-      for (let name of names) {
-        const stat = await fs.stat(path.join(this.dir, name))
-        const child = {
-          name,
-          timestamp: {
-            atime: stat.atime,
-            ctime: stat.ctime,
-            mtime: stat.mtime,
-            birthtime: stat.birthtime,
-          }
-        }
-        if (stat.isDirectory()) child.type = 'directory'
-        else if (stat.isFile()) child.type = 'file'
-        else if (stat.isSymbolicLink()) child.type = 'symbolic link'
+  parsePath(name) {
+    return path.parse(path.resolve(name))
+  }
 
-        this.stat.children.push(child)
-      }
-    } catch (err) {
-      throw err
+  async parseChildren(name) {
+    let children = await fs.readdir(name)
+    children = children.map(async (child) => await this.parse(path.join(name, child), { hasChildren: true }))
+    return await Promise.all(children)
+  }
+
+  async parse(name, options={}) {
+    let result = {}
+    result.path = this.parsePath(name)
+    const stat = await fs.stat(name)
+    result.timestamp = {
+      atime: stat.atime,
+      birthtime: stat.birthtime,
+      ctime: stat.ctime,
+      mtime: stat.mtime,
     }
+    if (!stat.isDirectory() || !options.hasChildren) return result
+    result.children = await this.parseChildren(name)
+    return result
   }
 
   async export() {
     try {
-      await this.parse()
+      this.stat = await this.parse(this.dir, { hasChildren: true })
       fs.writeFile(path.join(this.dir, '.dirstat'), JSON.stringify(this.stat, null, 2))
     } catch (err) {
       throw err
